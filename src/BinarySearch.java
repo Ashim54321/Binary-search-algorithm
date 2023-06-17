3ea325b79fd3562245eba5d4d19fd561b5d8bc47
Ashim gotame <ashimgotame123@gmail.com>
2023-06-17 17:15:33 +0545

    Binary search algorithm

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,5 +1,23 @@
 public class BinarySearch {
     public static void main(String[] args) {
+       int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,23,34,45,56,67,78,89,90};
+       int target = 23;
+        System.out.println(binarysearch(arr,target));
+    }
 
+    static int binarysearch(int[] arr, int target){
+          int start= 0;
+          int end = arr.length -1;
+          while (start <= end){
+              int mid = start + (end-start)/2;
+              if(target < arr[mid]){
+                  end = mid -1;
+              } else if (target > arr[mid]) {
+                  start = mid + 1;
+              }else {
+                  return mid;
+              }
+          }
+          return -1;
     }
 }
